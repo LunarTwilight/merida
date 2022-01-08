736b3de8c12eadd529d53fe9946356393e9bc971
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2022-01-07 23:44:40 -0900

    + comments so I can bump the version to test the new action for publishing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const colors = require('ansi-colors');
 
+/* Inject red color for first line of error */
 const originalConsoleError = console.error;
 const error = (...args) => {
     const error = (args[0].stack ? args[0].stack : args[0]).split('\n');
@@ -10,6 +11,7 @@ const error = (...args) => {
     return originalConsoleError.call(console, error);
 }
 
+/* Color first line of warning yellow (maybe would be fine just coloring the whole thing yellow?) */
 const originalConsoleWarn = console.warn;
 const warn = (...args) => {
     const warning = args[0].split('\n');
@@ -17,9 +19,11 @@ const warn = (...args) => {
     return originalConsoleWarn.call(console, warning);
 }
 
+/* Export as functions for importing */
 module.exports.warn = warn;
 module.exports.error = error;
 
+/* Export as a function that can be called to override native console.* functions */
 module.exports.init = () => {
     console.warn = warn;
     console.error = error;
